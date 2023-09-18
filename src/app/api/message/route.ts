786d929e4cca340639c5b306fbf52f97b788022c
erratinsilentio <kacper.kondracki@wp.pl>
2023-09-18 10:14:27 +0200

    stream communication with chatgpt is written

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,4 +1,8 @@
-import { ChatGPTMessage } from "@/lib/openai-stream";
+import {
+  ChatGPTMessage,
+  OpenAIStream,
+  OpenAIStreamPayload,
+} from "@/lib/openai-stream";
 import { MessageArraySchema } from "@/lib/validators/message";
 import { chatbotPrompt } from "@/utils/chatbotPrompt";
 
@@ -19,4 +23,20 @@ export async function POST(req: Request) {
     role: "system",
     content: chatbotPrompt,
   });
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -17,7 +17,7 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ messages: message }),
+        body: JSON.stringify({ messages: [message] }),
       });
       return response.body;
     },
